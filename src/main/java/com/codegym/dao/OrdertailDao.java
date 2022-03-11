1dbe9bbb9afbc2b989c9bb6d4cd9985b2653dcf8
chiendh2cm1 <chiendh2cm1@gmail.com>
2022-03-11 11:53:54 +0700

    add DBconec

--- src/main/java/com/codegym/dao/OrdertailDao.java
+++ src/main/java/com/codegym/dao/OrdertailDao.java
@@ -5,7 +5,6 @@ import com.codegym.model.Orderdetail;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class OrdertailDao {
